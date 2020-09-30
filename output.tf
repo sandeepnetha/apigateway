9987c1d5ffcb882411f8b4054b25825789da89ec
sandeepnetha <sandeepnetha.gajula@gmail.com>
2020-09-30 17:53:06 +0530

    output variables

---
+++ output.tf
@@ -0,0 +1,30 @@
+output "invoke_url" {
+  value = aws_api_gateway_deployment.apigw.invoke_url
+}
+
+output "execution_arn" {
+  value = aws_api_gateway_deployment.apigw.execution_arn
+}
+
+output "deployment_created_date" {
+  value = aws_api_gateway_deployment.apigw.created_date
+}
+
+output "deployment_id" {
+  value = aws_api_gateway_deployment.apigw.id
+}
+output "api_id" {
+  value = aws_api_gateway_rest_api.apigw.id
+}
+
+output "api_root_id" {
+  value = aws_api_gateway_rest_api.apigw.root_resource_id
+}
+
+output "gateway_http_method" {
+  value = aws_api_gateway_method.main.http_method
+}
+
+output "gateway_resource_id" {
+  value = aws_api_gateway_method.main.resource_id
+}
